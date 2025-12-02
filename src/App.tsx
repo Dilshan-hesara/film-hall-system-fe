efd37c2252f7d2d10e94f24ce1bca1ffbb0acac1
Dilshan-hesara <hesarsdilshan@gamil.com>
2025-12-02 20:08:21 +0530

    add Home route to main application

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import AddMovie from './pages/admin/AddMovie'; // Import කරන්න
+import AddMovie from './pages/admin/AddMovie';
+import Home from './pages/Home'; 
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />  {/* Main Home Page */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} /> {/* Route */}
         {/* <Route path="/" element={<Home />} /> */}
