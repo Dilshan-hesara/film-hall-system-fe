f3c39830abf14eb2353943f69e1914f311942b81
Dilshan-hesara <hesarsdilshan@gamil.com>
2025-12-03 19:43:20 +0530

    add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // 👈 මේක අකුරක් නෑර මෙහෙම තියෙන්න ඕන
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
