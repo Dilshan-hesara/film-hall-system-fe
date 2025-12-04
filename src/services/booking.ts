66dd7d2f6859754a8dfdecee93da023856eaf986
Dilshan-hesara <hesarsdilshan@gamil.com>
2025-12-04 20:20:20 +0530

    feat: update getOccupiedSeats to include hallId parameter for improved seat availability queries

--- src/services/booking.ts
+++ src/services/booking.ts
@@ -1,9 +1,20 @@
 import api from './api';
 
-export const getOccupiedSeats = async (movieId: string, date: string, time: string) => {
-  // Query params 
-  const response = await api.get(`/bookings/occupied?movieId=${movieId}&date=${date}&time=${time}`);
-  return response.data; // "A1", "A2"
+// export const getOccupiedSeats = async (movieId: string, date: string, time: string) => {
+//   // Query params 
+//   const response = await api.get(`/bookings/occupied?movieId=${movieId}&date=${date}&time=${time}`);
+//   return response.data; // "A1", "A2"
+// };
+
+// export const createBooking = async (bookingData: any) => {
+//   const response = await api.post('/bookings/create', bookingData);
+//   return response.data;
+// };
+
+export const getOccupiedSeats = async (movieId: string, date: string, time: string, hallId: string) => {
+  // Query hallId 
+  const response = await api.get(`/bookings/occupied?movieId=${movieId}&date=${date}&time=${time}&hallId=${hallId}`);
+  return response.data;
 };
 
 export const createBooking = async (bookingData: any) => {
