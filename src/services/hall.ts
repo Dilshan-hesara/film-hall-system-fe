8f9a5c59f6d1ad0f76cbd55516eeb9decf82e39c
Dilshan Hesara <hesarsdilshan@gmail.com>
2025-12-16 18:03:26 +0530

    consolidate hall-related API functions

--- src/services/hall.ts
+++ src/services/hall.ts
@@ -21,3 +21,18 @@ export const getHallById = async (id: string) => {
   const response = await api.get(`/halls/${id}`);
   return response.data;
 };
+
+export const updateHall = async (id: string, hallData: HallData) => {
+  const response = await api.put(`/halls/${id}`, hallData);
+  return response.data;
+};
+
+export const deleteHall = async (id: string) => {
+  const response = await api.delete(`/halls/${id}`);
+  return response.data;
+};
+
+export const getHalls = async () => {
+  const response = await api.get('/halls/all');
+  return response.data;
+};
