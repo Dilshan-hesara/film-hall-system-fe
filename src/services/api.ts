48c933a86228cf40be58eb74b16fc8bbc4682b24
Dilshan Hesara <hesarsdilshan@gmail.com>
2026-01-05 16:30:05 +0530

    Update API base URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -64,7 +64,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/v1',
+  // baseURL: 'http://localhost:5000/api/v1',
+  baseURL: 'https://film-hall-system-be.vercel.app/api/v1',
 });
 
 api.interceptors.request.use((config) => {
